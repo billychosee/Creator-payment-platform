00686495163efe967782b4f4b2dea8ec055375ea
Billy Kadyamatimba <billy@smatechgroup.com>
2025-11-16 16:53:08 +0200

    Update: Configuration enhancements for Next.js project setup

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
+
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
